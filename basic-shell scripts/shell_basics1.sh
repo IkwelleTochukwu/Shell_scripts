e888dafd205619f89ca599e2eb39135f739a1b2c
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-14 16:58:12 +0100

    count down with shell script

---
+++ basic-shell scripts/shell_basics1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a comment on the script
+# Write a script that uses a for loop to count from 3 to 7.
+
+for i in 3,4,5,6,7
+do
+    echo Counting down from 3 to 7, now at $i
+done
+
