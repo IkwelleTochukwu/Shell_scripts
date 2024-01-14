77a67ec89a28aa61c6e0302c524c4134b8278648
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-14 17:28:54 +0100

    Wraping an if statement around the script so it is also correct when there are no .txt files

--- basic-shell scripts/shell_basics2.sh
+++ basic-shell scripts/shell_basics2.sh
@@ -3,9 +3,14 @@
 
 i=0
 for file in *.txt;do
-        if [ -f "$file" ]; then
-                ((i++))
+        if [ -f "$file" ]; then    # to check that the .txt found is a file
+                ((i++))  # increment file number
         fi
 done
+if [ $i -gt 0 ]; then
+    echo "There are $i file ending in .txt in the current directory"   # print if the number of .txt file is greater than 0
+else
+    echo "There are no .txt files in this directory"  # print in .txt file is 0
+fi
 
-echo "There are $i file ending in .txt in the current directory"
+
