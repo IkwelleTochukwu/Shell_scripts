91f785cdfca07f90a682128f9ed0ba96c6d91abe
IkwelleTochukwu <tochiantho99@gmail.com>
2024-01-16 04:13:53 +0100

    added code to validate that input is a number

--- basic-shell scripts/shell_function7.sh
+++ basic-shell scripts/shell_function7.sh
@@ -7,6 +7,13 @@ read number1
 echo -n "Enter number2: "
 read number2
 
+# To check if the input is numeric
+if ! [[ $number1 =~ ^[0-9]+$ ]] || ! [[ $number2 =~ ^[0-9]+$ ]]; then
+    echo "Enter a valid number!"
+    exit 1
+fi
+
+# funtion to run the evaluation
 function evaluate {
     let sum="$number1+$number2"
     let prod="$number1*$number2"
